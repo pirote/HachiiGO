b2108a3d9207e59b56aa9e45d1a7b3ad8abb5158
AumChayanon <52564954+AumChayanon@users.noreply.github.com>
2020-08-17 18:35:07 +0700

    Update App.vue

--- hachii_go/src/App.vue
+++ hachii_go/src/App.vue
@@ -3,13 +3,7 @@
     <div class="row" style="padding:10px">
       <div class="col" style="max-width:20%;">
         <a class="navbar-brand" href="#">
-          <img
-            src=""
-            height="30"
-            id="logo"
-            class="d-inline-block align-top"
-            alt
-          />
+          <img src height="30" id="logo" class="d-inline-block align-top" alt />
         </a>
       </div>
       <div class="col" style="max-width:55%; text-align: center;">
@@ -170,6 +164,7 @@
     </div>
 
     <router-view />
+    <p>nameLine: {{nameLine}} idLine:{{idLine}} imgLine:{{imgLine}}</p>
     <div id="aboutus">
       <div class="row">
         <div class="col">
@@ -218,9 +213,8 @@
 <script>
 import firebase from "firebase";
 import Swal from "sweetalert2";
-import liff from '@line/liff';
+import liff from "@line/liff";
 export default {
-
   data() {
     return {
       status: "default",
@@ -237,83 +231,54 @@ export default {
       picture: null,
       uploadValue: 0,
       line_profile: {
-        userId: '',
-        displayName: '',
-        pictureUrl: '',
-        statusMessage: ''
-    },
-    nameLine:'',
-    imgLine:'',
-    idLine:''
+        userId: "",
+        displayName: "",
+        pictureUrl: "",
+        statusMessage: "",
+      },
+      nameLine: "",
+      imgLine: "",
+      idLine: "",
     };
   },
-  beforeCreate(){
-  liff
+  beforeCreate() {
+    liff
       .init({ liffId: "1654665014-qlP8X7Wd" })
-      .then(function(){
-          //window.alert('this on OS:' + liff.getOS);
+      .then(function () {
+        //window.alert('this on OS:' + liff.getOS);
       })
-      .catch(function(error) {
-            window.alert('Error init msg: ' + error);
-        });
+      .catch(function (error) {
+        window.alert("Error init msg: " + error);
+      });
   },
   async created() {
     await firebase.auth().onAuthStateChanged((firebaseUser) => {
       this.uid = firebaseUser.email;
-      
     });
-      if (!liff.isInClient()) {
-        /*liff.sendMessages([{
+
+    if (!liff.isInClient()) {
+      /*liff.sendMessages([{
             'type': 'text',
             'text': "heelo" 
         }])*/
     } else {
-        
-        liff.getProfile()
-          .then(profile=>{
-            const name =profile.displayName
-            //const line_email = liff.getDecodedIDToken().email
-            this.nameLine = profile.displayName
-            this.imgLine = profile.pictureUrl
-            this.idLine = profile.userId
-            //const line_Uid = profile.userId
-            //const line_PUrl = profile.pictureUrl
-            // merge profile line to database
-            if(this.nameLine){
-              firebase
-                .auth()
-                .signInWithEmailAndPassword(this.nameLine + '@line.com', this.idLine)
-                .then(
-                  (data) => {
-                    this.status = "default";
-                    this.uid = data.user.uid;
-                  },
-                  (err) => {
-                    firebase
-                      .auth()
-                      .createUserWithEmailAndPassword(this.nameLine + '@line.com', this.idLine)
-                      .then((data) => {
-                        data.user
-                          .updateProfile({
-                            displayName: this.nameLine,
-                            photoURL: this.idLine,
-                          })
-                          .then(() => {});
-                      })
-                      .catch((err) => {
-                        this.error = err.message;
-                      });
-                    console.log(err);
-                  }
-                );
-
-            }
-            liff.sendMessages([{
-              'type': 'text',
-              'text': 'hello ' + name+
-            }])
-          })
-        /*liff.sendMessages([{
+      liff.getProfile().then((profile) => {
+        const name = profile.displayName;
+        const line_email = liff.getDecodedIDToken().email;
+        this.nameLine = profile.displayName;
+        this.imgLine = profile.pictureUrl;
+        this.idLine = profile.userId;
+        //const line_Uid = profile.userId
+        //const line_PUrl = profile.pictureUrl
+        // merge profile line to database
+        liff.sendMessages([
+          {
+            type: "text",
+            text: "hello " + name + " " + line_email,
+          },
+        ]);
+      });
+      /*liff.sendMessages([{
             'type': 'text',
             'text': 'hello ' + profile_line.
         }]).then(function() {
@@ -322,12 +287,61 @@ export default {
             window.alert('Error sending msg: ' + error);
         });*/
     }
+    setTimeout(() => this.loginLine(), 1000);
+
     console.log(this.uid);
-    if(await this.uid === ''){
-      Swal.fire('ลงทะเบียนแล้วเข้าสู่ระบบกับเราสิ!')
+    if ((await this.uid) === "") {
+      Swal.fire("ลงทะเบียนแล้วเข้าสู่ระบบกับเราสิ!");
     }
   },
   methods: {
+    loginLine() {
+      if (this.nameLine) {
+        firebase
+          .auth()
+          .signInWithEmailAndPassword(this.nameLine + "@line.com", this.idLine)
+          .then(
+            (data) => {
+              this.status = "default";
+              this.uid = data.user.uid;
+            },
+            (err) => {
+              firebase
+                .auth()
+                .createUserWithEmailAndPassword(
+                  this.nameLine + "@line.com",
+                  this.idLine
+                )
+                .then((data) => {
+                  data.user
+                    .updateProfile({
+                      displayName: this.nameLine,
+                      photoURL: this.idLine,
+                    })
+                    .then(() => {
+                      firebase
+                        .auth()
+                        .signInWithEmailAndPassword(this.nameLine + "@line.com", this.idLine)
+                        .then(
+                          (data) => {
+                            this.status = "default";
+                            this.uid = data.user.uid;
+                          },
+                          (err) => {
+                            alert("ับัญชี หรือ รหัสผ่าน ไม่ถูกต้อง!");
+                            console.log(err);
+                          }
+                        );
+                    });
+                })
+                .catch((err) => {
+                  this.error = err.message;
+                });
+              console.log(err);
+            }
+          );
+      }
+    },
     logout() {
       firebase
         .auth()
@@ -378,22 +392,22 @@ export default {
         );
     },
     async previewImage(event) {
-      if(this.imageData){
-      await firebase
-        .storage()
-        .ref(`${this.imageData.name}`)
-        .delete()
-        .then(function () {
-          this.picture = null;
-        })
-        .catch(function (error) {
-          console.log(error);
-          // Uh-oh, an error occurred!
-        });}
+      if (this.imageData) {
+        await firebase
+          .storage()
+          .ref(`${this.imageData.name}`)
+          .delete()
+          .then(function () {
+            this.picture = null;
+          })
+          .catch(function (error) {
+            console.log(error);
+            // Uh-oh, an error occurred!
+          });
+      }
       this.uploadValue = 0;
       this.imageData = event.target.files[0];
 
-      
       const storageRef = firebase
         .storage()
         .ref(`${this.imageData.name}`)
@@ -535,10 +549,10 @@ body {
   }
 }
 @media only screen and (max-width: 1024px) {
-  #aboutus{
+  #aboutus {
     font-size: 10px;
   }
-  li{
+  li {
     font-size: 10px;
   }
 }
